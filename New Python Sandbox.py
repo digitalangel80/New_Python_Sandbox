1a0ac372b6154c28d78d5ca7199349e153a69f36
digitalangel80 <angel@re-solve.bg>
2023-10-22 00:59:03 +0300

    Second attempt

--- New Python Sandbox.py
+++ New Python Sandbox.py
@@ -2,3 +2,10 @@ lottery_numbers = {3, 13, 12, 34, 23, 32}
 
 print(lottery_numbers)
 
+players = [
+{1,2,3,4,5}, {3, 5, 6, 7, 23}
+]
+
+print(players[0].intersection(lottery_numbers))
+
+print("This is a test of making a commit and syncing in the GutHub")
